18aeba6262d2765e8ed4834c88365b4e004d3b56
Sky McKinley <sky.mckinley@humboldt.edu>
2020-01-10 16:41:18 -0800

    Added housing app extract to stage SQL file in sql/ directory.

---
+++ sql/hsg_apps_extr_to_stg.sql
@@ -0,0 +1,17 @@
+insert into 
+    stg_oie_housing_apps
+select 
+    to_number(entryapplicationid) application_id,
+    substr(hsuid, 1, 9) emplid,
+    entryname applicant_name,
+    termdescription residence_session,
+    classificationdescription1 student_category,
+    applicationstatusdescription2 application_status,
+    to_date(replace(receiveddate,'nan',''), 'MM/DD/YYYY HH24:MI') app_rcvd_dttm,
+    trunc(to_date(replace(finalcomplete,'nan',''), 'MM/DD/YYYY HH24:MI')) app_completed_date,
+    trunc(to_date(replace(offersentdate,'nan',''), 'MM/DD/YYYY HH24:MI')) offer_sent_date,
+    trunc(to_date(replace(canceldate,'nan',''), 'MM/DD/YYYY HH24:MI')) app_cancel_date,
+    'HSNG' data_origin,
+    systimestamp created_ew_dttm,
+    'N' loaded
+from extr_oie_housing_apps
