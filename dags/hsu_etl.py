2b4c4f86256a59589295e663d259d9ddcc40d683
Sky McKinley <sky.mckinley@humboldt.edu>
2020-01-14 08:41:40 -0800

    Initial commit of utility class python file.

---
+++ dags/hsu_etl.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# HSU ETL utitility class
+import hashlib
+
+class HSU_ETL:
+
+    def __hash_preprocess(self, value):
+        value_type = type(value)
+
+        typemap = {
+            'date': 'date',
+            'str': value,
+            'number': 'number',
+            'integer': 'integer',
+            'decimal': 'decimal',
+            'long': 'long',
+            'boolean': 'boolean',
+            'byte': 'byte',
+            'cbyte': 'cybte'
+                }
+        return(typemap.get(value_type, '-'))
+
+    def hash(self, *args):
+        hash_string = ''.join(str(args))
+
+        return hashlib.md5(hash_string).hexdigest()
