6b66f9a38db836424b77edc258eb6f902d98162c
Sky McKinley <sky.mckinley@humboldt.edu>
2020-04-16 09:50:58 -0700

    Added dummy operators for basic student, instructor, and enrollment data and set dependencies.

--- dags/students.py
+++ dags/students.py
@@ -37,8 +37,20 @@ dag = DAG(
    catchup=True
 )
 
+download_programs = DummyOperator(
+        task_id = 'download_programs',
+        dag = dag)
+
 download_students = DummyOperator(
         task_id = 'download_students',
+        dag = dag)
+
+download_classes = DummyOperator(
+        task_id = 'download_classes',
+        dag = dag)
+
+download_instructors = DummyOperator(
+        task_id = 'download_instructors',
         dag = dag)
 
 download_enrollment = DummyOperator(
@@ -49,10 +61,25 @@ load_students = DummyOperator(
         task_id = 'load_students',
         dag = dag)
 
+load_instructors = DummyOperator(
+        task_id = 'load_instructors',
+        dag = dag)
+
+load_classes = DummyOperator(
+        task_id = 'load_classes',
+        dag = dag)
+
 load_enrollment = DummyOperator(
         task_id = 'load_enrollment',
         dag = dag)
 
-load_students.set_upstream(download_students)
-load_enrollment.set_upstream(download_enrollment)
-load_enrollment.set_upstream(load_students)
+download_students >> load_students
+download_programs >> load_students
+download_instructors >> load_instructors
+download_classes >> load_classes
+
+download_enrollment >> load_enrollment
+
+load_classes >> load_enrollment
+load_students >> load_enrollment 
+load_instructors >> load_enrollment
